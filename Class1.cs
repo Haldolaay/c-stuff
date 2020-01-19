26e8af5a97408e1daade4b6a17c442e5d8a3680e
hamzah <haldolaay@gmail.com>
2020-01-18 21:17:05 -0800

    validate binary tree

--- Class1.cs
+++ Class1.cs
@@ -6,7 +6,22 @@ public class Class1
 	{
 		
 	}
-		static void Main() {
-			Console.WriteLine("hello");
-		}
+	
+}
+
+class Node {
+	public int value;
+	public Node right;
+	public Node left;
+		
+}
+public boolean isTrue(Node n) {
+
+	return isTrue(n, Integer.MIN_VALUE, Integer.MAX_VALUE);
+
+}
+public boolean isTrue(Node n, int min, int max) {
+	if (n == null) return true;
+	if (n < min || n > max) return false;
+	return isTrue(n.left, min, n.value) && isTrue(n.right, n.value + 1, max);
 }
